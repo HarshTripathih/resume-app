ac00054be080d421bb1a88ad103d89d10b4bb967
HarshTripathih <harshtripathih321@gmail.com>
2022-11-16 13:53:00 +0530

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,48 +7,43 @@ function Navbar() {
 
 
     return (
-        <div className="navbar_container fixed-top">
-            <div className="navbar_left">{name}</div>
-            <div className="navbar_right">
-
-                <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
-                    <div className="container-fluid">
-                    <a class="navbar-brand navbar-danger" href="#">{name}</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">About</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Experience</a>
-                                </li>
-                                <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Tools
-                                    </a>
-                                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a className="dropdown-item" href="#">Resume</a></li>
-                                        <li><a className="dropdown-item" href="#">Projects</a></li>
-                                        <li><a className="dropdown-item" href="#">Contact</a></li>
-                                        <li><hr className="dropdown-divider" />Download Resume</li>
-                                        <li><a className="dropdown-item" href="#">Download</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                            <form className="d-flex">
-                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                <button className="btn btn-outline-success " type="submit">Search</button>
-                            </form>
-                        </div>
+        <div className="container">
+            <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light flex-row-reverse">
+                <div className="container-fluid">
+                    <a className="fs-1 navbar-brand text-danger" href="#">{name}</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Link</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
                     </div>
-                </nav>
-            </div>
+                </div>
+            </nav>
         </div>
     );
 }
